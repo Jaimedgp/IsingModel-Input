16ab57b6e1e6e84168d7fc81bc400e1ae08a7f93
InesMovellan <45602328+InesMovellan@users.noreply.github.com>
2018-12-05 19:59:15 +0100

    Main para correr el InPut.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InPut ines = new InPut();
+		
+		ines.write("hola.txt");
+	}
+
+}

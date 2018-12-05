df408b581204bb0bb8af0e76777bafc7ecf194d2
InesMovellan <45602328+InesMovellan@users.noreply.github.com>
2018-12-05 18:44:22 +0100

    InPut class with coments
    Lo he retocado un pelín para que esté bonito, queda solo un error dentro del parse cuando se invoca al método nameVar.equalsIgnoreCase(names) que me da miedo enredar. Voy a intentar arreglarlo pero lo subo así por si la cago. 
    Lo he ordenado de la siguiente forma: 
    - Atributos
    - Constructor (vacío)
    - Parse
    - Read
    - Write
    
    With so much love
    
    Inesita

--- InPut.java
+++ InPut.java
@@ -1,108 +1,101 @@
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-class InPut {
+/**
+ * Container of all the information needed to perform the simulation. It reads the data 
+ * from the text file passed as parameter.
+ * 
+ * @author Jaime Díez González-Pardo
+ * @author Pablo Lavín Pellón
+ * @author Inés Sánchez de Movellán Sáiz
+ * 
+ */
 
-	private String[] names = {"temperature", "latticeLength", "J", "mcs", "therm", "skip", "H"};
+public class InPut {
+	
 	/**
-	 *    * Temperature of the system expressed in reduced units.
-	 *       */
+	 * Array of strings in which each element is the name of one variable
+	 */
+	private String[] names = {"temperature", "latticeLength", "J", "mcs", "therm", "skip", "H"};
+	
+	/**
+	 * Temperature of the system expressed in reduced units.
+	 */       
 	public double temperature = 0.9;
 
 	/**
-		*    * An integer value with the length of the side of the lattice square.
-		*       */
+	 * An integer value with the length of the side of the lattice square.
+	 */
 	public int latticeLength = 40;
 
 	/**
-	*    * Number of different interaction parameters that correspond to the levels (numer of distances) 
-	*       * at which the magnetization of the spins of the particles there located will be considered as 
-	*          * able to interact which the one of the position under analysis. 
-	*             */
+	 * Number of different interaction parameters that correspond to the levels (number of distances) 
+	 * at which the magnetization of the spins of the particles there located will be considered as 
+	 * able to interact which the one of the position under analysis. 
+	 */
 	public int nJ = 2;
 
 	/**
-		*    * An array with as many interaction constants as levels of neighbors to 
-		*       * consider, as indicated by the nJ attribute.
-		*          */
+	 * An array with as many interaction constants as levels of neighbors to 
+	 * consider, as indicated by the nJ attribute.
+	 */
 	public double[] J = { 1.0 , 0.0 };
 
 	/**
-	*    * Number of "Montecarlo" sweeps to do, i.e. the number of iterations
-	*       */
+	 * Number of "Montecarlo" sweeps to do, i.e. the number of iterations
+	 */
 	public int mcs = 100000;
 
 	/**
-		*    * Number of iteration that will be considered as needed for the system to 
-		*       * be in thermal equilibrium. Also called number of thermalization sweeps.
-		*          * It is the number of sweeps to perform before starting taking samples.
-		*             */
+	 * Number of iteration that will be considered as needed for the system to 
+	 * be in thermal equilibrium. Also called number of thermalization sweeps.
+	 * It is the number of sweeps to perform before starting taking samples.
+	 */
 	public int therm = 25000;
 
 	/**
-	*    * This number minus one corresponds to the number of Montecarlo 
-	*       * sweeps to skip from sample to sample. The simulation will take 
-	*          * one sample every this number of  sweeps.
-	*             */
+	 * This number minus one corresponds to the number of Montecarlo 
+	 * sweeps to skip from sample to sample. The simulation will take 
+	 * one sample every this number of sweeps.
+	 */
 	public int skip = 1000;
 
 	/**
-	*    * This is the external magnetic field that will affect the lattice. It is 
-	*       * unidimentional since it is considered to be in the direction of the spins analyzed.
-	*          */
+	 * This is the external magnetic field that will affect the lattice. It is 
+	 * unidimentional since it is considered to be in the direction of the spins analyzed.
+	 */
 	public double H = 0.0;
-
+	
+	
+	/**
+	 * Constructor of the class InPut
+	 */
 	public InPut() {
+		
 	}
-
-	  /**
-	   * Fill the attributes of the InputData object with those read from the file given as 
-	   * argument.  It returns the number of values that could be read from the file, zero 
-	   * if no data could be taken from the file, or a negative number if the file does not 
-	   * exist or could not be open for reading.
-	   * 
-	   * @param fileFullName a string with th complete path an name of a file to be read.
-	   * @return it returns the number of values that could be read from the file, zero if no 
-	   * data could be taken from the file or a negative number if there is no file or t is not 
-	   * possible to open it.
-	   */
-	public int read(String filename) {
-
-		try {
-				BufferedReader fr = new BufferdReader(new FileReade(filename))
-
-				String line;
-				boolean isCorrect;
-				int numLine = 0;
-				
-				while ((line = fr.readline()) != null) {
-					numLine++;
-					
-					isCorrect = parse(line);
-					// It returns the number of the line which has a mistake (in this case 
-					// isCorrect = false)
-					if (!isCorrect) return numLine;
-					
-				}
-				return 0;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+	
 	
 	/**
 	  * This method reads the string provided and add the values that can be 
 	  * understood from it into the object used to invoke it.
 	  * 
-	  * @param line a string to be analyzed, read and the balues written in it stored
+	  * @param line a string to be analyzed, read and the values written in it stored
 	  * 
-	  * @returns boolean true if the value can be understood, false ion other case
+	  * @returns boolean true if the value can be understood, false in other case
 	  */
-	public boolean parse (String line){
+	public boolean parse (String line){		
+		// It creates an array of two string, one for the name of the variable (temperature, 
+		// latticeLength etc.) and the other one for the value of the this variable)
 		
+		// For this version we suppose that every variable is dimensionless
+
 		String lineArray[] = new String[2];
+		// It separate the strings of the line by tabulator
 		lineArray = line.split("\t");
 		
 		String nameVar = lineArray[0];
@@ -110,10 +103,13 @@ class InPut {
 		// String unitsVar = lineArray[2];
 		
 		int variable;
-		for (variable = 0; variable < names.length(); variable++){
+		for (variable = 0; variable < names.length; variable++){
 			if (nameVar.equalsIgnoreCase(names)) break; 
 		}	
 		
+		// With switch-case loop, it assigns the value of the variable to the appropriate attribute
+		
+		// For this version we suppose that there are not errors in the input file
 		switch(variable) {
 			case 0:
 				this.temperature = Double.parseDouble(valueVar);
@@ -123,12 +119,9 @@ class InPut {
 				this.latticeLength = Integer.parseInt(valueVar);
 				break;
 				
-		//  case "nJ":
-		//  	this.nJ = Integer.parseInt("valueVar");
-		//  	return true;
-		//  	break;
-							
 			case 2:
+				// The first value of array of interaction parameters is an integer which is the 
+				// number of J, nJ
 				this.J[0] = Integer.parseInt(valueVar);
 				int nJ = (int) J[0];
 				for(int k = 1; k < J.length; k++) {
@@ -157,3 +150,80 @@ class InPut {
 		
 		return true;
 	  }
+	
+
+	/**
+	 * Fill the attributes of the InputData object with those read from the file given as 
+	 * argument.  It returns zero if it could read every data from the file, the number of
+	 * the line that it could not read if there is any, or a negative number if the file does not 
+	 * exist or could not be open for reading.
+	 * 
+	 * @param fileFullName a string with the complete path an name of a file to be read.
+	 * @return it returns zero if it could read every data from the file, the number of
+	 * the line that it could not read if there is any, or a negative number if the file does not 
+	 * exist or could not be open for reading.
+	 */
+	public int read(String fileFullName) {
+
+		try {	
+		        // It uses the class BufferedReader to read the input data file
+				BufferedReader fr = new BufferedReader(new FileReader(fileFullName));
+				
+				// It defines the line, a boolean which indicates the result of applied the parse 
+				// method and the number of the line what is reading
+				String line;
+				boolean isCorrect;
+				int numLine = 0;
+				
+				while ((line = fr.readLine()) != null) {
+					numLine++;
+					isCorrect = parse(line);
+					// It returns the number of the line which has a mistake (in this case 
+					// isCorrect = false)
+					if (!isCorrect) return numLine;
+				}
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return -1;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 0;
+	}
+	
+	
+	/**
+	 * It writes the values stored in the input data object into the file given as argument
+	 * 
+	 * @param fileFullName a string with the name of the file in which the data object will be printed
+	 */
+	public void write(String fileFullName) {
+		  
+		  try {
+			      // It uses the class BufferedWriter to write the values of the input data objects
+			      // in a file 
+			      BufferedWriter fw = new BufferedWriter(new FileWriter(fileFullName, true));
+			      
+			      fw.write("Temperature: " + temperature);
+			      fw.write("Lattice Length: " + latticeLength);
+			      fw.write("Number of interaction parameters" + J[0]);
+			      for(int k = 1; k < J.length; k++){
+			    	  fw.write("J"+k+J[k]);
+			      }
+			    
+			      fw.write("MCS: " + mcs);
+			      fw.write("Therm: " + therm);
+			      fw.write("Skip: " + skip);
+			      fw.write("H: " + H);
+			    
+		   	   } catch (FileNotFoundException e) {
+		   		   e.printStackTrace();
+		   		   System.out.println("The file has not been found");
+		   	   }catch (IOException e) {
+		   		   e.printStackTrace();
+		   		   System.out.println("The input/output operation has failed");
+		   	   }
+	    }
+}
